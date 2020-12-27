de029c4bb99a60cd79515043cbeecc36dc56e2a7
Karel Fourie <karelmakazi@gmail.com>
2020-12-28 10:38:46 +1300

    Remove redux

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,13 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-import { fetchFruits } from '../actions'
+function App(){
 
-export class App extends React.Component {
-  state = {
-    fruits: []
-  }
-
-  componentDidMount () {
-    this.props.dispatch(fetchFruits())
-  }
-
-  render () {
-    return (
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {this.props.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
+  return(
+    <div className=''>
+      <h1> Quiz 2000</h1>
+      <h3>Welcome to Quiz 2000. Please select your category from the options below.</h3>
+    </div>
+  )
 }
 
-function mapStateToProps (globalState) {
-  return {
-    fruits: globalState.fruits
-  }
-}
-
-export default connect(mapStateToProps)(App)
+export default App
