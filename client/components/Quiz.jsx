2aced612d392d8c0c896f9e33d91e46aeec187b4
Karel Fourie <karelmakazi@gmail.com>
2021-04-21 11:04:54 +1200

    WIP Reset

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -6,16 +6,13 @@ import Answer from './Answer'
 
 // LAYOUT STYLING
 const quizPanel =
-  'grid grid-cols-4 gap-2 auto-rows-max justify-center m-auto w-396 md:w-3/4 h-5/6 p-7 rounded-lg border-4 border-greenMid shadow-xl bg-black text-center'
-const headingGridBlock = 'pb-3 md:pb-7 col-span-full row-span-3'
-const optionsGridBlock =
-  'col-span-full row-span-3 flex flex-col md:flex-row md:flex-wrap pt-4 border-t-2 border-greenMid'
+  'grid grid-cols-4 gap-2 grid-rows-6 justify-center m-auto w-3/4 h-5/6 p-7 rounded-lg border-4 border-greenMid shadow-lg bg-black '
 
-// TYPOGRAPHY
-const heading =
-  'font-black uppercase text-greenMid text-7xl md:text-8xl lg:text-9xl'
-const byLine =
-  'px-4 md:px-24 lg:px-16  xl:px-44 text-2xl md:text-3xl lg:text-4xl leading-tight md:leading-snug text-textWhite'
+const categoryGridBlock = 'bg-blueMid col-span-full row-span-2'
+const questionGridBlock = 'bg-blueMid col-span-full row-span-2'
+const answerGridBlock = 'bg-greenMid col-span-full row-span-2 flex flex-col md:flex-row md:flex-wrap'
+
+//TYPOGRAPHY
 
 function Quiz ({ category }) {
   const [questionList, setQuestionList] = useState('wait')
@@ -28,44 +25,41 @@ function Quiz ({ category }) {
   }, [])
 
   useEffect(() => {
+    console.log(answerStatus)
     answerStatus ? setScore(score + 1) : setScore(score)
   }, [questionIndex])
 
   return (
     <div className={quizPanel}>
 
-      <div className={headingGridBlock}>
-        <div>
-          <h1 className={heading}>{category.catName}</h1>
-          <h3 className={byLine}>Score: {score}</h3>
-        </div>
+      <div className={categoryGridBlock}>
+        <h1>{category.catName}</h1>
+        <h3>Score: {score}</h3>
       </div>
 
-      <div className={optionsGridBlock}>
-        {questionIndex < 20 && (
-          <>
-            <div className='questionGridBlock'>
-              <h3 className={byLine}>{questionList[questionIndex].question}</h3>
-            </div>
+      {questionIndex < 20 && (
+        <>
+          <div className={questionGridBlock}>
+            <h3>{questionList[questionIndex].question}</h3>
+          </div>
 
-            <div className='answerGridBlock'>
-              {questionList !== 'wait' &&
-              questionList[questionIndex].answerArray.map((answer, index) => {
-                return (
-                  <div key={index} className="quiz-stack__answer-hstack">
-                    <Answer
-                      questionIndex={questionIndex}
-                      answer={answer}
-                      setQuestionIndex={setQuestionIndex}
-                      setAnswer={setAnswer}
-                    />
-                  </div>
-                )
-              })}
-            </div>
-          </>
+          <div className={answerGridBlock}>
+            {questionList != 'wait' &&
+            questionList[questionIndex].answerArray.map((answer, index) => {
+              return (
+                <div key={index} className="quiz-stack__answer-hstack">
+                  <Answer
+                    questionIndex={questionIndex}
+                    answer={answer}
+                    setQuestionIndex={setQuestionIndex}
+                    setAnswer={setAnswer}
+                  />
+                </div>
+              )
+            })}
+          </div>
+        </>
       )}
-      </div>
 
       {questionIndex === 20 && (
         <div className="quiz-report">
