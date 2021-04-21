ddaebabfbdfc127e06bb48b0de2c03210a898475
Karel Fourie <karelmakazi@gmail.com>
2021-04-21 16:10:32 +1200

    WIP Iintial Styling complete

--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -6,24 +6,30 @@ import Answer from './Answer'
 
 // LAYOUT STYLING
 const quizPanel =
-'grid grid-cols-4 gap-2 auto-rows-max justify-center m-auto w-396 md:w-3/4 h-5/6 p-7 rounded-lg border-4 border-greenMid shadow-xl bg-black text-center'
+  'grid grid-cols-4 gap-2 auto-rows-max justify-center m-auto w-396 md:w-3/4 h-5/6 p-7 rounded-lg border-4 border-greenMid shadow-xl bg-black text-center'
 
-const categoryGridBlock = 'col-span-full row-span-3 pb-2 md:pb-4 border-b-2 border-greenMid'
+const categoryGridBlock =
+  'col-span-full row-span-3 pb-2 md:pb-4 border-b-2 border-greenMid'
 
-const questionGridBlock = 'col-span-full row-span-2 py-10 border-b-2 border-greenMid'
-const answerGridBlock =
-  'col-span-full row-span-2 flex flex-col md:flex-row md:flex-wrap'
+const questionGridBlock =
+  'col-span-full row-span-2 py-10 border-b-2 border-greenMid'
+const answerGridBlock = 'col-span-full row-span-2 flex flex-col'
 
 // TYPOGRAPHY
 const categoryName =
   'font-black uppercase text-greenMid text-6xl md:text-7xl lg:text-8xl'
-const scoreAlert =
-  'text-2xl md:text-3xl lg:text-4xl text-textWhite'
-const questionBody =
-'px-4 md:px-24 lg:px-16  xl:px-44 text-2xl md:text-3xl lg:text-4xl leading-tight md:leading-snug text-textWhite'
+const scoreAlert = 'text-2xl md:text-3xl lg:text-4xl text-textWhite'
+const tryAgain =
+  'pt-12 text-2xl md:text-3xl lg:text-4xl text-greenMid transition ease-out duration-500 hover:text-textWhite'
 
+const quizReport = 'pt-12 text-3xl md:text-4xl lg:text-5xl text-textWhite'
+
+// ELEMENTS
 const answerButton =
-  'flex-grow h-14 md:h-24 md:w-2/4 uppercase leading-tight text-4xl text-greenMid transition ease-out duration-500 hover:text-textWhite cursor-pointer select-none focus:outline-none '
+  'flex-grow h-12 uppercase leading-tight text-3xl text-greenMid transition ease-out duration-500 hover:text-textWhite cursor-pointer select-none focus:outline-none'
+
+const questionBody =
+  'px-4 md:px-24 lg:px-16  xl:px-44 text-2xl md:text-3xl lg:text-4xl leading-tight md:leading-snug text-textWhite'
 
 function Quiz ({ category }) {
   const [questionList, setQuestionList] = useState('wait')
@@ -49,7 +55,9 @@ function Quiz ({ category }) {
       {questionIndex < 20 && (
         <>
           <div className={questionGridBlock}>
-            <h3 className={questionBody}>{questionList[questionIndex].question}</h3>
+            <h3 className={questionBody}>
+              {questionList[questionIndex].question}
+            </h3>
           </div>
 
           <div className={answerGridBlock}>
@@ -71,9 +79,13 @@ function Quiz ({ category }) {
       )}
 
       {questionIndex === 20 && (
-        <div className="quiz-report">
-          <h3>You have completed the quiz with a score of {score}</h3>
-          <Link to={'/'}>Home</Link>
+        <div className={answerGridBlock}>
+          <h3 className={quizReport}>
+            You have completed the quiz with a score of {score}
+          </h3>
+          <Link to={'/'}>
+            <h3 className={tryAgain}>Try Again</h3>
+          </Link>
         </div>
       )}
     </div>
