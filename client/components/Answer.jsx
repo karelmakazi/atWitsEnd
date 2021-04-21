f23f790f4cfe99da198de2050cf2b28a1d3e2ba0
Karel Fourie <karelmakazi@gmail.com>
2021-04-21 13:46:00 +1200

    WIP Quiz styling in progress

--- client/components/Answer.jsx
+++ client/components/Answer.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
-// ELEMENT STYLING
-const categoryButton =
-  'flex-grow h-14 md:h-24 md:w-2/4 uppercase leading-tight text-2xl text-greenMid transition ease-out duration-500 hover:text-textWhite cursor-pointer select-none focus:outline-none '
+const answerButton = 'focus:outline-none'
 
 function Answer ({ answer, setAnswer, questionIndex, setQuestionIndex }) {
   const processAnswer = () => {
@@ -12,7 +10,7 @@ function Answer ({ answer, setAnswer, questionIndex, setQuestionIndex }) {
 
   return (
     <div>
-      <button className={categoryButton} onClick={() => processAnswer()}>{answer.answer}</button>
+      <button className={answerButton}onClick={() => processAnswer()}>{answer.answer}</button>
     </div>
   )
 }
--- client/components/Quiz.jsx
+++ client/components/Quiz.jsx
@@ -6,13 +6,24 @@ import Answer from './Answer'
 
 // LAYOUT STYLING
 const quizPanel =
-  'grid grid-cols-4 gap-2 grid-rows-6 justify-center m-auto w-3/4 h-5/6 p-7 rounded-lg border-4 border-greenMid shadow-lg bg-black '
+'grid grid-cols-4 gap-2 auto-rows-max justify-center m-auto w-396 md:w-3/4 h-5/6 p-7 rounded-lg border-4 border-greenMid shadow-xl bg-black text-center'
 
-const categoryGridBlock = 'bg-blueMid col-span-full row-span-2'
-const questionGridBlock = 'bg-blueMid col-span-full row-span-2'
-const answerGridBlock = 'bg-greenMid col-span-full row-span-2 flex flex-col md:flex-row md:flex-wrap'
+const categoryGridBlock = 'col-span-full row-span-3 pb-2 md:pb-4 border-b-2 border-greenMid'
 
-//TYPOGRAPHY
+const questionGridBlock = 'col-span-full row-span-2 py-10 border-b-2 border-greenMid'
+const answerGridBlock =
+  'col-span-full row-span-2 flex flex-col md:flex-row md:flex-wrap'
+
+// TYPOGRAPHY
+const categoryName =
+  'font-black uppercase text-greenMid text-6xl md:text-7xl lg:text-8xl'
+const scoreAlert =
+  'text-2xl md:text-3xl lg:text-4xl text-textWhite'
+const questionBody =
+'px-4 md:px-24 lg:px-16  xl:px-44 text-2xl md:text-3xl lg:text-4xl leading-tight md:leading-snug text-textWhite'
+
+const answerButton =
+  'flex-grow h-14 md:h-24 md:w-2/4 uppercase leading-tight text-4xl text-greenMid transition ease-out duration-500 hover:text-textWhite cursor-pointer select-none focus:outline-none '
 
 function Quiz ({ category }) {
   const [questionList, setQuestionList] = useState('wait')
@@ -25,38 +36,36 @@ function Quiz ({ category }) {
   }, [])
 
   useEffect(() => {
-    console.log(answerStatus)
     answerStatus ? setScore(score + 1) : setScore(score)
   }, [questionIndex])
 
   return (
     <div className={quizPanel}>
-
       <div className={categoryGridBlock}>
-        <h1>{category.catName}</h1>
-        <h3>Score: {score}</h3>
+        <h1 className={categoryName}>{category.catName}</h1>
+        <h3 className={scoreAlert}>Score: {score}</h3>
       </div>
 
       {questionIndex < 20 && (
         <>
           <div className={questionGridBlock}>
-            <h3>{questionList[questionIndex].question}</h3>
+            <h3 className={questionBody}>{questionList[questionIndex].question}</h3>
           </div>
 
           <div className={answerGridBlock}>
-            {questionList != 'wait' &&
-            questionList[questionIndex].answerArray.map((answer, index) => {
-              return (
-                <div key={index} className="quiz-stack__answer-hstack">
-                  <Answer
-                    questionIndex={questionIndex}
-                    answer={answer}
-                    setQuestionIndex={setQuestionIndex}
-                    setAnswer={setAnswer}
-                  />
-                </div>
-              )
-            })}
+            {questionList !== 'wait' &&
+              questionList[questionIndex].answerArray.map((answer, index) => {
+                return (
+                  <div key={index} className={answerButton}>
+                    <Answer
+                      questionIndex={questionIndex}
+                      answer={answer}
+                      setQuestionIndex={setQuestionIndex}
+                      setAnswer={setAnswer}
+                    />
+                  </div>
+                )
+              })}
           </div>
         </>
       )}
@@ -67,7 +76,6 @@ function Quiz ({ category }) {
           <Link to={'/'}>Home</Link>
         </div>
       )}
-
     </div>
   )
 }
